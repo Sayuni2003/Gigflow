8bc3f642aa5b5d8ab9e74bc046d72171cfac2016
M S S Manju Sri <sayuni3manjusri@gmail.com>
2026-07-04 12:35:21 +0530

    Revert "Feature/order management"

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRoutes.js";
 import userRouter from "./userRoutes.js";
-import gigRouter from "./gigRoutes.js";
-import orderRouter from "./orderRoutes.js";
 
 const apiRouter = Router();
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/users", userRouter);
-apiRouter.use("/gigs", gigRouter);
-apiRouter.use("/orders", orderRouter);
 
 export default apiRouter;
